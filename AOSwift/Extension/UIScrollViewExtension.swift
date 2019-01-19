6339b73cf32e0c419762fcd0a5b861e61727d5d8
artwebs <arwebs@126.com>
2019-01-19 12:37:09 +0800

    完成部分列表开发

--- AOSwift/Extension/UIScrollViewExtension.swift
+++ AOSwift/Extension/UIScrollViewExtension.swift
@@ -23,4 +23,63 @@ extension UIScrollView{
         self.setContentOffset(CGPoint(x: posx, y: 0), animated: true)
     }
     
+    struct PullToRefreshConst {
+        static let tag = 810
+        static let alpha = true
+        static let height: CGFloat = 80
+        static let imageName: String = "pulltorefresharrow.png"
+        static let animationDuration: Double = 0.4
+        static let fixedTop = true // PullToRefreshView fixed Top
+    }
+    
+    class PullToRefreshOption {
+        var backgroundColor = UIColor.clear
+        var indicatorColor = UIColor.gray
+        var autoStopTime: Double = 0.7 // 0 is not auto stop
+        var fixedSectionHeader = false  // Update the content inset for fixed section headers
+    }
+    
+//    fileprivate var pullToRefreshView: PullToRefreshView? {
+//        get {
+//            let pullToRefreshView = viewWithTag(PullToRefreshConst.tag)
+//            return pullToRefreshView as? PullToRefreshView
+//        }
+//    }
+//    
+//    func addPullToRefresh(_ refreshCompletion :@escaping (() -> ())) {
+//        self.addPullToRefresh(PullToRefreshOption(), refreshCompletion: refreshCompletion)
+//    }
+//    
+//    func addPullToRefresh(_ options: PullToRefreshOption = PullToRefreshOption(), refreshCompletion :@escaping (() -> ())) {
+//        let refreshViewFrame = CGRect(x: 0, y: -PullToRefreshConst.height, width: self.frame.size.width, height: PullToRefreshConst.height)
+//        let refreshView = PullToRefreshView(options: options, frame: refreshViewFrame, refreshCompletion: refreshCompletion)
+//        refreshView.setTag(PullToRefreshConst.tag)
+//        addSubview(refreshView)
+//    }
+//    
+//    func startPullToRefresh() {
+//        pullToRefreshView?.state = .refreshing
+//    }
+//    
+//    func stopPullToRefresh() {
+//        pullToRefreshView?.state = .normal
+//    }
+//    
+//    // If you want to PullToRefreshView fixed top potision, Please call this function in scrollViewDidScroll
+//    func fixedPullToRefreshViewForDidScroll() {
+//        if PullToRefreshConst.fixedTop {
+//            if self.contentOffset.y < -PullToRefreshConst.height {
+//                if var frame = pullToRefreshView?.frame {
+//                    frame.origin.y = self.contentOffset.y
+//                    pullToRefreshView?.frame = frame
+//                }
+//            } else {
+//                if var frame = pullToRefreshView?.frame {
+//                    frame.origin.y = -PullToRefreshConst.height
+//                    pullToRefreshView?.frame = frame
+//                }
+//            }
+//        }
+//    }
+    
 }
---
+++ AOSwift/View/ListViewRefresh.swift
@@ -0,0 +1,43 @@
+//
+//  ListViewRefresh.swift
+//  HongyuanProperty
+//
+//  Created by 刘洪彬 on 2019/1/19.
+//  Copyright © 2019 liu hongbin. All rights reserved.
+//
+
+import UIKit
+
+class ListViewRefresh: ListView {
+    var page=1, pageSize=5
+    fileprivate var refreshOperate :(_ page :Int ,_ pageSize :Int)->() = {page,pageSize in }
+    fileprivate var moreOperate:(_ page :Int ,_ pageSize :Int)->() = {page,pageSize in }
+    
+    func setRefreshOperate(_ opt : @escaping (_ page :Int ,_ pageSize :Int)->()){
+        self.refreshOperate=opt
+    }
+    
+    func setMoreOperate(_ opt : @escaping (_ page :Int ,_ pageSize :Int)->()){
+        self.moreOperate=opt
+    }
+    
+    override func draw(_ rect: CGRect) {
+        super.draw(rect)
+        let options = PullToRefreshOption()
+        options.backgroundColor = UIColor.white
+//        self.addPullToRefresh(options, refreshCompletion: { [weak self] in
+//            // some code
+//            self?.page=1
+//            self?.refreshOperate(self!.page,self!.pageSize)
+//        })
+    }
+    
+    /*
+    // Only override draw() if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func draw(_ rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
